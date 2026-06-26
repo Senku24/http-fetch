1063564a44f6360fffb5e2b4fbaa4697ed6de876
Senku24 <nixonpaule24@gmail.com>
2026-06-26 06:26:08 +0530

    ass1

--- ass1.js
+++ ass1.js
@@ -7,28 +7,61 @@ const app = express();
 const port = 3000;
 
 
-// Logger Middleware
-function requestLogger(req, res, next) {
+app.use(express.json());
+app.use( function(req, res, next) {
     const timestamp = new Date().toISOString();
+    const method = req.method;
+    const url = req.originalUrl;
+    req.requestInfo = {
+        timestamp: timestamp,
+        method: method,
+        url: url
+    };
+    next();
+    })
 
-    console.log(
-        `[${timestamp}] ${req.method} ${req.originalUrl}`
-    );
+app.get("/", function(req,res){
+    res.sendFile("/Users/nixonpaul/Code_learn/WebDev26/http-server/http-re/ass1.html");
+})
 
-    next(); 
-}
-
-// Register middleware
-app.use(requestLogger);
-
-app.get("/", (req, res) => {
-    res.send("Welcome!");
+app.get("/request-info", function(req, res) {
+    const requestInfo = req.requestInfo;
+    res.json(requestInfo);
 });
+app.listen(port)
 
-app.get("/users", (req, res) => {
-    res.send("Users List");
-});
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+
+
+
+
+
+
+
+
+
+// // Logger Middleware
+// function requestLogger(req, res, next) {
+//     const timestamp = new Date().toISOString();
+
+//     console.log(
+//         `[${timestamp}] ${req.method} ${req.originalUrl}`
+//     );
+
+//     next(); 
+// }
+
+// // Register middleware
+// app.use(requestLogger);
+
+// app.get("/", (req, res) => {
+//     res.send("Welcome!");
+// });
+
+// app.get("/users", (req, res) => {
+//     res.send("Users List");
+// });
+
+// app.listen(port, () => {
+//     console.log(`Server running on port ${port}`);
+// });
